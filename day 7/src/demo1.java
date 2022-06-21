0361fbe19c5f6d6a8712f9d7f18999cea421f590
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-21 09:45:29 +0100

    day7 done

--- day 7/src/demo1.java
+++ day 7/src/demo1.java
@@ -1,4 +1,18 @@
 
 public class demo1 {
+	package com.ank.constructor;
+	class age {
+
+	    int age;
+	    age(int age){
+	        this.age = age;
+	    }
+
+	    public static void main(String[] args) {
+	        age x = new age(23);
+	        System.out.println("my age is = " + x.age);
+	    }
+	}
+	
 
 }
